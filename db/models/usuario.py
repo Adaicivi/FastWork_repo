e2f9b3f6eba72b8142d705aaceaad5b0658e4c40
Alvaro Galvão Filho <alvaro.galvao.filho1580@gmail.com>
2025-06-29 19:50:36 -0300

    alteração para criaçào de empregadores

--- db/models/usuario.py
+++ db/models/usuario.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from .profissao import Profissao
 from .endereco import Endereco
 from .imagem import Imagem
+from typing import Optional
 
 @dataclass
 class Usuario:
@@ -10,11 +11,11 @@ class Usuario:
     email : str
     senha : str
     data_nascimento : str
-    imagem : Imagem
-    exp : str
+    imagem : Optional[Imagem] = None
+    exp : Optional[str] = None
     cpf : str
     telefone : str
-    link_contato : str
+    link_contato : Optional[str] = None
     endereco : Endereco
-    profissao : Profissao
-    tipo : str
+    profissao : Optional[Profissao] = None
+    tipo : Optional[str] = None
